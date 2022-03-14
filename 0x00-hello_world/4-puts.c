6a4e64579f733a2ee181fc732962cccb0476bc65
Ayeoritsetomi <tomiuweja@gmail.com>
2022-03-14 16:18:07 -0700

    mandatory task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,9 @@
  *
  * Return: Always 0 (success)
  */
-        int main(void)
-        {
-        puts("\n\"Programming is like building a multilingual puzzle\n");
+int main(void)
+{
+	puts("\n\"Programming is like building a multilingual puzzle\n");
 
-        return (0);
-        }
+	return (0);
+}	
